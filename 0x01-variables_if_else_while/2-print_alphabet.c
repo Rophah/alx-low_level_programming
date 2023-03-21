2fc4f560f4c2557351a64a15ddcd5459fd525427
Rophah-alx <aderafat@gmail.com>
2023-03-20 19:32:41 -0700

    print the alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,14 @@
 /**
  * main function
  *
- * Return: alway 0
+ * Return: always 0
  */
 
 int main(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= 'z'; i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
