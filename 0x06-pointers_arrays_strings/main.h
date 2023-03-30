e037812f8112ece4314e6235bae82da71dc73a45
Rophah-alx <aderafat@gmail.com>
2023-03-30 15:50:03 -0700

    task 11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ char *leet(char *n);
 char *rot13(char *s);
 void print_number(int n);
 void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
