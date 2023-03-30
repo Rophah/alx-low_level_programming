4bad306268d78410ec87526cdfaec497972977ad
Rophah-alx <aderafat@gmail.com>
2023-03-30 13:30:16 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#incude "main.h"
+
+/**
+ * _strcmp - compare string
+ * @s1: strings
+ * @s2: strings
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0 ; s1 != '\0' ||  s2 != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
